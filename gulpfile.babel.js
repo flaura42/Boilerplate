99ef64ed1368a8ad496a5846929fcefa12e4a3d0
Laura <flaura42@gmail.com>
2019-05-24 14:09:36 -0700

    Add gulpfile and tester html/css files

---
+++ gulpfile.babel.js
@@ -0,0 +1,89 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import browserSync from 'browser-sync';
+import del from 'del';
+
+const server = browserSync.create();
+
+const paths = {
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'dist/scripts/'
+  },
+  styles: {
+    src: 'src/styles/**/*.css',
+    dest: 'dist/styles/'
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'dist/'
+  },
+  images: {
+    src: 'src/images/**/*.{jpg, jpeg, png, svg}',
+    dest: 'dist/images/'
+  }
+};
+
+/**********    Build tasks    **********/
+
+// Delete dist folder
+export const clean = () => del(['dist']);
+
+// Process and uglify js, copy to dist folder
+export function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Process and minify css, copy to dist folder
+export function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+// Copy html to dist folder
+export function html() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+
+/**********    Image Handling    **********/
+
+// Minify images and copy to build folder
+export function images() {
+  return gulp.src(paths.images.src, { since: gulp.lastRun(images)})
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+
+/**********    Development    **********/
+
+// Start serving src folder
+export function serve(done) {
+  server.init({
+    server: {
+      baseDir: './src'
+    }
+  });
+  done();
+}
+
+// Reload the server
+export function reload(done) {
+  server.reload();
+  done();
+}
+
+// Watch files for changes and reload server
+export function watch() {
+  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+  gulp.watch(paths.html.src, gulp.series(html, reload));
+}
+
+// Build dist folder, start server, and watch files
+const build = gulp.series(clean, gulp.parallel(scripts, styles, html, images), serve, watch);
+
+// Set build as default task
+export default build;
